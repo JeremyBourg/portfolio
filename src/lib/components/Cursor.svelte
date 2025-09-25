6e0ead4e1f1177c7a310a57bfa1ceeec5b4ac959
Jeremy Bourg <jeremy.bourg@outlook.com>
2025-09-25 00:02:52 -0400

    refactor cursor code

--- src/lib/components/Cursor.svelte
+++ src/lib/components/Cursor.svelte
@@ -1,42 +1,40 @@
 <script>
 import { Spring } from 'svelte/motion';
-import { onMount } from 'svelte';
 
 const coords = new Spring({ x: 50, y: 50 }, {
-    stiffness: 0.15, damping: 0.25
+    stiffness: 0.25, damping: 0.64
 });
 
 const size = new Spring(15);
 
-onMount(() => {
-    if(typeof window !== 'undefined') {
-        window.addEventListener('mousemove', (e) => {coords.target = {x: e.clientX, y: e.clientY}}, { passive: true });
-        window.addEventListener('mousedown', () => {size.target = 20}, { passive: true });
-        window.addEventListener('mouseup', () => {size.target = 15}, { passive: true });
-    }
-});
+const mousemove = (e) => { coords.target = {x: e.clientX, y: e.clientY}, { passive: true }};
+const mousedown = () => { size.target = 20, { passive: true }};
+const mouseup = () => { size.target = 15, { passive: true }};
+
 </script>
 
-<div>
-    <svg role="presentation" width="100%" height="100%">
-        <circle
-            cx={coords.current.x}
-            cy={coords.current.y}
-            r={size.current}
-        />
-    </svg>
-</div>
+<svelte:window 
+    on:mousemove={mousemove}
+    on:mousedown={mousedown}
+    on:mouseup={mouseup}
+/>
+
+<svg role="presentation">
+    <circle
+        cx={coords.current.x}
+        cy={coords.current.y}
+        r={size.current}
+    />
+</svg>
 
 <style>
-div {
+svg {
     position: fixed;
-    top: 0;
-    left: 0;
     width: 100%;
     height: 100%;
     pointer-events: none;
     mix-blend-mode: difference;
-    z-index: 100;
+    z-index: 9999;
 }
 
 circle {
