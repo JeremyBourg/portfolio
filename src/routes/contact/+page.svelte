9aa4fc91d4b7ee9a07382334b74ae6c3ff3a3ea3
Jeremy Bourg <jeremy.bourg@outlook.com>
2025-09-29 15:16:20 -0400

    contact page contents and styles
    TODO: functional form
    
    a

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,1 +1,131 @@
-<h1>contact</h1>
+<style>
+.wrapper > h1 {
+    padding-top: 100px;
+    margin-bottom: 50px;
+}
+
+form {
+    padding: 50px 0;
+
+    & > * + * {
+        margin-top: 20px;
+    }
+
+    * {
+        outline: none;
+    }
+}
+
+a {
+    font-size: var(--font-size-p);
+    color: var(--color-primary);
+    text-decoration: underline;
+}
+
+fieldset {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 20px;
+    border: none;
+    padding: 0;
+    margin: 0;
+
+    input, textarea {
+        padding: 16px;
+        background-color: transparent;
+        border: 4px var(--color-secondary) solid;
+        font-family: var(--font-secondary);
+        font-size: var(--font-size-p);
+        color: var(--color-primary);
+    }
+
+    input::placeholder, textarea::placeholder {
+        font-family: var(--font-secondary);
+        font-size: var(--font-size-p);
+        color: var(--color-tertiary);
+    }
+}
+
+textarea {
+    grid-column: 1 / -1;
+    height: 150px;
+    resize: vertical;
+}
+
+.info {
+    display: flex;
+    justify-content: space-between;
+}
+
+#submit {
+    cursor: pointer;
+    position: relative;
+    padding: 16px 32px;
+    border: 4px solid var(--color-secondary);
+    overflow: hidden;
+    display: inline-block;
+
+    & > * {
+        cursor: pointer;
+        font-family: var(--font-primary);
+        background-color: transparent;
+        border: 0;
+        color: var(--color-secondary);
+        font-size: var(--font-size-btn);
+        font-weight: 700;
+        letter-spacing: unset;
+        transition: color 0.3s ease-out;
+    }
+}
+
+#submit::before {
+	content: '';
+	position: absolute;
+	z-index: -1;
+	inset: -10px;
+	background-color: var(--color-secondary);
+
+	transform: translateX(-100%);
+	transition: transform 0.3s ease-out;
+}
+
+#submit:hover {
+    & > * {
+        color: var(--color-primary);
+    }
+
+    &::before {
+        transform: translateX(0);
+    }
+}
+</style>
+
+<div class="wrapper">
+    <h1>On se parle?</h1>
+    <p>Vous avez des questions ou un projet en tête? Je suis là pour transformer vos idées en réalité, sans bugs (ou presque)!</p>
+    <form action="">
+        <fieldset>
+            <input type="text" placeholder="Nom">
+            <input type="email" placeholder="Courriel">
+            <textarea id="message" placeholder="Message"></textarea>
+        </fieldset>
+        <div id="submit">
+            <input type="submit" value="Soumettre">
+        </div>
+    </form>
+
+    <div class="info">
+        <div>
+            <p>Courriel:</p>
+            <p>jer.bourg2@gmail.com</p>
+        </div>
+        <div>
+            <p>Téléphone:</p>
+            <p>(438) 518-7401</p>
+        </div>
+        <div>
+            <p>CV:</p>
+            <a href="/link/to/pdf" download>Télécharger (PDF)</a>
+        </div>
+    </div>
+</div>
