0dab20b0dce4825d46407f2db848511a3f4dec52
jeremy <jer.bourg2@gmail.com>
2025-10-05 22:42:09 -0400

    fix: rework parallax logic to use svelte builtins
    this incidentally fixed the bug of scrolling in another page and going
    back to home page at a random scroll position
    
    tmp
    
    tmp

--- src/lib/components/Parallax.svelte
+++ src/lib/components/Parallax.svelte
@@ -1,30 +1,25 @@
 <script>
 import parallax0 from '$lib/assets/parallax0.png'
-import parallax1 from '$lib/assets/parallax5.png'
-import parallax2 from '$lib/assets/parallax4.png'
+import parallax1 from '$lib/assets/parallax1.png'
+import parallax2 from '$lib/assets/parallax2.png'
 import parallax3 from '$lib/assets/parallax3.png'
-import parallax4 from '$lib/assets/parallax2.png'
-import parallax5 from '$lib/assets/parallax1.png'
+import parallax4 from '$lib/assets/parallax4.png'
+import parallax5 from '$lib/assets/parallax5.png'
 import name from '$lib/assets/parallax-name.png'
 
-let top = $state(0);
+const layers = [
+	parallax0,
+	parallax1,
+	parallax2,
+	parallax3,
+	parallax4,
+	parallax5,
+];
 
-function scroll(event) {
-    const layers = document.getElementsByClassName("parallax");
-    for (var i = 0; i < layers.length; i++) {
-        const layer = layers[i];
-        const speed = layer.getAttribute('data-speed');
-        const yPos = -(top * speed / 100);
-        layer.setAttribute('style', `transform: translateY(${yPos}px)`);
-        if (i == layers.length - 2) {
-            const fill = document.getElementById('fill')
-            fill.setAttribute('style', `height: ${yPos * -1}px`)
-        }
-    }
-};
+let top = $state();
 </script>
 
-<svelte:window on:scroll={scroll} bind:scrollY={top} />
+<svelte:window bind:scrollY={top} />
 
 <style>
 #parallax {
@@ -43,25 +38,6 @@ img {
     width: 100%;
     object-fit: cover;
 }
-.parallax-layer0 {
-    z-index: -10;
-}
-.parallax-layer1 {
-    z-index: -9;
-}
-.parallax-layer2 {
-    z-index: -8;
-}
-.parallax-layer3 {
-    z-index: -7;
-}
-.parallax-layer4 {
-    z-index: -6;
-}
-.parallax-layer5 {
-    z-index: -5;
-}
-
 .parallax-name {
     z-index: -10;
     top: 16vh;
@@ -69,12 +45,13 @@ img {
 </style>
 
 <div id="parallax">
-    <div id="fill"></div>
-    <img class="parallax parallax-layer0" src="{parallax0}" alt="" data-speed="0">
-    <img class="parallax parallax-layer1" src="{parallax1}" alt="" data-speed="10">
-    <img class="parallax parallax-layer2" src="{parallax2}" alt="" data-speed="25">
-    <img class="parallax parallax-layer3" src="{parallax3}" alt="" data-speed="36">
-    <img class="parallax parallax-layer4" src="{parallax4}" alt="" data-speed="49">
-    <img class="parallax parallax-layer5" src="{parallax5}" alt="" data-speed="100">
-    <img class="parallax parallax-name" src="{name}" alt="" data-speed="0">
+    <div id="fill" style="height: {top}px"></div>
+	{#each layers as image, index}
+		<img
+			src="{image}"
+			alt="parallax layer {index}"
+			style="transform: translate(0, {(-top * index) / (layers.length - 1)}px); z-index: {-10 + index}"
+		>
+	{/each}
+    <img class="parallax-name" src="{name}" alt="">
 </div>
