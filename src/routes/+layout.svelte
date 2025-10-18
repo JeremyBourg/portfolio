ed5dacbf2e7fd0d7257fbd378e085cd5e867a6da
jeremybourg <jer.bourg2@gmail.com>
2025-10-17 21:27:01 -0400

    fix: reset position on navigation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,29 +1,32 @@
 <script>
 	import favicon from '$lib/assets/favicon.svg';
+	import { navigating } from '$app/stores';
     import Cursor from '$lib/components/Cursor.svelte';
     import Footer from '$lib/components/Footer.svelte';
     import Header from '$lib/components/Header.svelte';
-	import { gsap } from 'gsap';
+	import { gsap } from 'gsap/dist/gsap';
 	import { ScrollSmoother } from 'gsap/ScrollSmoother';
 	import { ScrollTrigger } from 'gsap/ScrollTrigger';
-	import { onMount } from 'svelte';
 
     import '$lib/styles/main.scss'
 
-	let smoother;
+	gsap.registerPlugin(ScrollSmoother, ScrollTrigger);
 	let { children } = $props();
 
-	if (typeof window !== undefined) {
-		gsap.registerPlugin(ScrollSmoother, ScrollTrigger);
-	}
-
-	onMount(() => {
-		smoother = ScrollSmoother.create({
+	$effect(() => {
+		const smoother = ScrollSmoother.create({
 			scroll: 1,
 			effects: true,
 			smoothTouch: 0.1,
 		});
-	})
+		if ($navigating === null && smoother) {
+			smoother.refresh();
+		}
+		else if (smoother) {
+			smoother.scrollTo(0);
+		}
+	});
+
 </script>
 
 <svelte:head>
@@ -35,9 +38,7 @@
 
 <div id="smooth-wrapper">
 	<div id="smooth-content">
-
 		{@render children?.()}
-
 		<Footer />
 	</div>
 </div>
