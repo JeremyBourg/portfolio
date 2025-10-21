1f4397094d931a160af7fd116a418401b1950678
jeremybourg <jer.bourg2@gmail.com>
2025-10-21 04:24:31 -0400

    404 page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,3 +1,30 @@
-<!-- TODO: 404 -->
+<script>
+import CTA from "$lib/components/CTA.svelte";
+let { data } = $props();
+</script>
 
-404 page not done
+<div class="wrapper">
+	<div>
+		<h1>404</h1>
+		<p>Cette page n'existe pas.</p>
+
+		<br>
+		<CTA href="/" label="Retour à l'accueil" />
+	</div>
+</div>
+
+<style>
+
+.wrapper {
+	display: flex;
+	height: 62vh;
+	align-items: center;
+	justify-content: center;
+
+	text-align: center;
+}
+h1 {
+	margin-top: 4rem;
+	color: var(--color-secondary);
+}
+</style>
